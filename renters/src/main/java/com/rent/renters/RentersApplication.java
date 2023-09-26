a28d91a607ccc14526bd18b3b083b51cd83f603b
Nwad-Rob <rnwadinobi@gmail.com>
2023-09-26 09:01:06 -0500

    First Commit

---
+++ renters/src/main/java/com/rent/renters/RentersApplication.java
@@ -0,0 +1,13 @@
+package com.rent.renters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentersApplication.class, args);
+	}
+
+}
---
+++ renters/src/test/java/com/rent/renters/RentersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rent.renters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
